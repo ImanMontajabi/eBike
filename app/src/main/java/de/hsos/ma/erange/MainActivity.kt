af28bec97ea2c51eefabf57fef74cbe0a38b1335
Iman Montajabi <iman.montajabi@gmail.com>
2025-10-27 02:13:52 +0100

    Add Calculate button

--- app/src/main/java/de/hsos/ma/erange/MainActivity.kt
+++ app/src/main/java/de/hsos/ma/erange/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -32,24 +34,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val weight = rememberSaveable { mutableStateOf("80") }
+            val capacity = rememberSaveable { mutableStateOf("670") }
+            val isFlatTourProfile = rememberSaveable { mutableStateOf }
             ERangeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ERange(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        weight = weight,
+                        capacity = capacity
                     )
                 }
             }
         }
     }
-}
-
-@Composable
-fun ERange(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 
@@ -62,7 +60,7 @@ fun InputBox(title: String, txt: MutableState<String>) {
     {
         Column(
             Modifier
-                .padding(0.dp, 2.dp, 4.dp, 0.dp)
+                .padding(10.dp, 2.dp, 4.dp, 0.dp)
                 .weight(2F),
             horizontalAlignment = Alignment.End
         )
@@ -89,20 +87,57 @@ fun InputBox(title: String, txt: MutableState<String>) {
 }
 
 @Composable
+fun CalculateButton(
+    weight: MutableState<String>,
+    capacity: MutableState<String>,
+    output: MutableState<String>) {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Button(
+            onClick = {
+                val w = weight.value.toDoubleOrNull() ?: 0.0
+                val c = capacity.value.toDoubleOrNull() ?: 0.0
+                val r = range(w, c, true)
+                output.value = "Range is: %.1f km".format(r)
+        },
+            modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    "Calculate",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.inversePrimary
+                )
+        }
+    }
+}
+
+@Composable
 fun ERange(modifier: Modifier, weight: MutableState<String>, capacity: MutableState<String>) {
+    val output = rememberSaveable { mutableStateOf("") }
     Column(modifier.padding(10.dp))
     {
         InputBox("Your weight [kg]: ", txt = weight)
         Spacer(Modifier.requiredHeight(10.dp))
         InputBox("Battery capacity [Wh]: ", txt = capacity)
         Spacer(Modifier.requiredHeight(10.dp))
+        CalculateButton(weight, capacity, output)
+        if (output.value.isNotEmpty()) {
+            Text(
+                output.value,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
     }
 }
 
-fun range() {
-    // Wh per km
-    val compution = 7.0f
-
+fun range(weight: Double, capacity: Double, isFlat: Boolean): Double {
+    val consumption = 7.0 /* Wh per km */
+    val normWeight = 80 /* kg */
+    var range = (capacity / consumption * (normWeight / weight))
+    range = range / 2.0
+    if (!isFlat) range *= 0.7.toFloat() /* penalty mountainous */
+    return range
 }
 
 @Preview(showBackground=true)
